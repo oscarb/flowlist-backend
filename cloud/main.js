e8b341042e9844c833828d2c4493796294920cd1
oscarb <git@oscarb.se>
2017-08-17 22:04:59 +0200

    Get authorization token and respond with session token for new users

--- cloud/main.js
+++ cloud/main.js
@@ -16,102 +16,116 @@ Parse.Cloud.define("test", function(request, response) {
 
 
 
-Parse.Cloud.define("requestAccessToken", function(request, response) {
-  let authorizationCode = request.params.authorizationCode;
-  console.log("Authorization code: " + authorizationCode);
-  console.log("Node " + process.version);
-  getAccessToken(authorizationCode, function(data) {
-    console.log("RESPONSE: " + data);
-    const userAccount = JSON.parse(data);
-    validateUserAccessToken(userAccount.access_token);
-    // TODO Check if it is possible to send an object (and recieve a HashMap..)
-    response.success(data);
-  });
+Parse.Cloud.define("requestSessionToken", function(request, response) {
+  const authorizationCode = request.params.authorizationCode;
+
+  getUserAccessToken(authorizationCode)
+    .then(accessTokenData => accessTokenData.access_token)
+    .then(accessToken => validateUserAccessToken(accessToken))
+    .then(account => validateAppId(account))
+    .then(account => loginUser(account))
+    .then(user => { response.success(user.getSessionToken()) })
+    .catch(error => {
+      console.error("Error (" + error.code + "): " + error.message);
+      response.error("Error (" + error.code + "): " + error.message);
+    });
+
 });
 
 
-function validateUserAccessToken(token) {
-  // URL GET https://graph.accountkit.com/v1.2/me/?access_token=<access_token>
-  const appSecretProof = crypto.createHmac('sha256', facebookConfig.APP_SECRET)
-                                .update(token)
-                                .digest('hex');
-  const url = 'https://graph.accountkit.com/v1.2/me/?access_token=' + token + '&appsecret_proof=' + appSecretProof;
-  console.log("Validate token at " + url)
+const getUserAccessToken = function(authorizationCode) {
+  return new Promise((resolve, reject) => {
+    const facebookAppId = facebookConfig.APP_ID;
+    const appSecret =  facebookConfig.APP_SECRET;
+    const url = "https://graph.accountkit.com/v1.2/access_token?grant_type=authorization_code&code=" + authorizationCode + "&access_token=AA|" + facebookAppId + "|" + appSecret;
 
-  get(url)
-    .then(data => loginUser(JSON.parse(data)))
-    .catch(err => console.error(err));
-}
+    console.log("GET ACCESS TOKEN AT " + url);
 
-function loginUser(account) {
-  console.log("ACCOUNT", account)
-  if(account.application.id != facebookConfig.APP_ID) return;
+    get(url)
+      .then(data => resolve(JSON.parse(data)))
+      .catch(error => reject(error));
+  })
+};
 
-  getSessionToken(account);
-}
+const validateUserAccessToken = function(token) {
+  return new Promise((resolve, reject) => {
+    const appSecretProof = getAppSecretProof(token);
+    const url = 'https://graph.accountkit.com/v1.2/me/?access_token=' + token + '&appsecret_proof=' + appSecretProof;
+    console.log("VALIDATE TOKEN " + token + " AT " + url);
 
-function getSessionToken(account) {
+    get(url)
+      .then(data => resolve(JSON.parse(data)))
+      .catch(error => reject(error));
+  })
+};
 
-  // TODO If user with phone number does not exist, sign up user and store information, otherwise just login
-  // TODO Get session token and return to client
+const validateAppId = function(account) {
+  return new Promise((resolve, reject) => {
+    console.log("VALIDATE APP ID " + account.application.id + " WITH " + facebookConfig.APP_ID)
+    if (account.application.id == facebookConfig.APP_ID) {
+      return resolve(account);
+    } else {
+      return reject(new Error('Failed to verify application id, got ' + account.application.id + ', should be ' + facebookConfig.APP_ID));
+    }
+  })
+};
 
-  // Save phoneNumber, phoneCountryPrefix and accountKitId
+const loginUser = function(account) {
+  return new Promise((resolve, reject) => {
+    console.log("FIND USER WITH PHONE NUMBER " + account.phone.number);
 
-  // https://stackoverflow.com/questions/24582031/parse-com-how-to-login-as-a-user-without-knowing-their-password
-  // http://blog.parse.com/announcements/bring-your-own-login/
-  // https://stackoverflow.com/questions/28240493/parse-create-save-new-user-without-signup
-  // https://stackoverflow.com/questions/29489008/parse-why-does-user-getsessiontoken-return-undefined
-  // https://stackoverflow.com/questions/37754290/parse-server-user-getsessiontoken-not-defined
-
-  console.log("Looking for user with phone number ", account.phone.number);
-  let query = new Parse.Query(Parse.User);
-  query.equalTo("phoneNumber", account.phone.number);
-  query.first()
-    .then(user => {
+    let query = new Parse.Query(Parse.User);
+    query.equalTo("phoneNumber", account.phone.number);
+    query.first()
+      .then(user => {
       if(user) {
+        console.log("USER EXISTS IN SYSTEM " + user.get('username'));
+        return resolve(user);
         // Found user, try to get session
       } else {
         // No user found, sign up user
-        let user = new Parse.User();
-        user.set("accountKitId", account.id);
-        user.set("phoneNumber", account.phone.number);
-        user.set("phoneCountryPrefix", account.phone.country_prefix);
-        user.set("username", getRandomUsername());
-        user.set("password", getRandomPassword(32));
-
-        console.log("Random username " + getRandomUsername());
-        console.log("Random password " + getRandomPassword(32))
-
-        user.signUp(null).then(user => {
-          // TODO Do something with signed up user
-          console.log("Signed up user, session token" + user.getSessionToken() );
-
-        }).catch(error => console.error("Error: " + error.code + " " + error.message));
+        return signUpUser(account);
 
       }
-    }).catch(error => console.error(error));
+    }).catch(error => reject(error));
+  })
+};
 
+const signUpUser(account) = function(account) {
+  return new Promise((resolve, reject) => {
+    console.log("SIGN UP USER");
 
+    let user = new Parse.User();
+    user.set("accountKitId", account.id);
+    user.set("phoneNumber", account.phone.number);
+    user.set("phoneCountryPrefix", account.phone.country_prefix);
+    user.set("username", getRandomUsername());
+    user.set("password", getRandomPassword(32));
+
+    return user.signUp(null);
+  })
+};
+
+function getAppSecretProof(token) {
+  return crypto.createHmac('sha256', facebookConfig.APP_SECRET)
+    .update(token)
+    .digest('hex');
 }
 
-function getAccessToken(authorizationCode, callback) {
-  const facebookAppId = facebookConfig.APP_ID;
-  const appSecret =  facebookConfig.APP_SECRET;
-//  let graphUrl = url.parse('https://graph.accountkit.com');
-//  graphUrl.pathname = '/v1.2/access_token';
-//  graphUrl.query['grant_type'] = 'authorization_code';
-//  graphUrl.query['code'] =  authorizationCode;
-//  graphUrl.query['access_token'] = 'AA|' + facebookAppId + '|' + appSecret;
-//
-  const url = "https://graph.accountkit.com/v1.2/access_token?grant_type=authorization_code&code=" + authorizationCode + "&access_token=AA|" + facebookAppId + "|" + appSecret;
+// https://stackoverflow.com/questions/30309496/multiple-queries-with-parse-cloud-code-using-promises
+// https://stackoverflow.com/questions/31747100/parse-cloud-code-chain-promises
+// https://stackoverflow.com/questions/18229041/use-own-promises-in-parse-cloud-code
+// https://github.com/petkaantonov/bluebird/wiki/Promise-anti-patterns
+// https://gist.github.com/domenic/3889970
 
-  console.log("Getting URL: " + url);
+// https://stackoverflow.com/questions/24582031/parse-com-how-to-login-as-a-user-without-knowing-their-password
+// http://blog.parse.com/announcements/bring-your-own-login/
+// https://stackoverflow.com/questions/28240493/parse-create-save-new-user-without-signup
+// https://stackoverflow.com/questions/29489008/parse-why-does-user-getsessiontoken-return-undefined
+// https://stackoverflow.com/questions/37754290/parse-server-user-getsessiontoken-not-defined
 
-  get(url)
-    .then((html) => callback(html))
-    .catch((err) => console.error(err));
 
-}
+
 
 // https://www.tomas-dvorak.cz/posts/nodejs-request-without-dependencies/
 const get = function(url) {
@@ -122,7 +136,7 @@ const get = function(url) {
     const request = lib.get(url, (response) => {
       // handle http errors
       if (response.statusCode < 200 || response.statusCode > 299) {
-        reject(new Error('Failed to load page, status code: ' + response.statusCode));
+        reject(new Error('Failed to load ' + url + ' status code: ' + response.statusCode));
       }
       // temporary data holder
       const body = [];
